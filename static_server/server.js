30d42210bfb94762aefb18bdec46776b1232a601
suzukaze <nfs.suzuakze@gmail.com>
2020-11-22 14:29:14 +0800

    static server

---
+++ static_server/server.js
@@ -0,0 +1,68 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    response.statusCode = 200
+    const filePath = path === '/' ? '/index.html' : path
+    const fileTypes = {
+        "html": 'text/html',
+        "css": 'text/css',
+        "js": 'text/javascript',
+        "xml": 'text/xml',
+        "json": 'application/json',
+        "png": 'image/png',
+        "jpg": 'image/jpeg'
+    }
+    let content
+    try {
+        content = fs.readFileSync(`./public${filePath}`)
+    } catch(error) {
+        content = `no such file`
+        response.statusCode = 404
+    }
+    response.setHeader('Content-Type', `${fileTypes[filePath.slice(filePath.search(/(?<=\.).*/))] || 'text/html'};charset=utf-8`)
+    response.write(content)
+    response.end()
+    // if (path === '/') {
+    //     response.statusCode = 200
+    //     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    //     response.write(fs.readFileSync('./public/index.html'))
+    //     response.end()
+    // } else if (path === '/style.css') {
+    //     response.statusCode = 200
+    //     response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    //     response.write(fs.readFileSync('./public/style.css'))
+    //     response.end()
+    // } else if (path === '/main.js') {
+    //     response.statusCode = 200
+    //     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    //     response.write(fs.readFileSync('./public/main.js'))
+    //     response.end()
+    // } else {
+    //     response.statusCode = 404
+    //     response.end()
+    // }
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
